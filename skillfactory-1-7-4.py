9f414437ea40175b78a0e2453f2b3516f9c2bb1d
elinfinito <61184861+elinfinito@users.noreply.github.com>
2020-03-01 02:13:51 +0300

    Update skillfactory-1-7-4.py

--- skillfactory-1-7-4.py
+++ skillfactory-1-7-4.py
@@ -1,14 +1,14 @@
 text = 'Название языка питон произошло вовсе не от вида пресмыкающихся. Автор назвал язык в честь популярного британского комедийного телешоу 1970-х Летающий цирк Монти Пайтона'
-    text = text.split()
-    words_to_find = ['питон', 'Монти', 'проверка', 'честь', 'шоу', 'драма']
-    print("")
+text = text.split()
+words_to_find = ['питон', 'Монти', 'проверка', 'честь', 'шоу', 'драма']
+print("")
 
-    result = set() 
+result = set() 
 
-    for i in text:
-        if i in words_to_find:
-            result.add(i)
-        else:
-            continue
-    result = sorted(result)  
+for i in text:
+    if i in words_to_find:
+        result.add(i)
+    else:
+       continue
+result = sorted(result)  
     print('Следующие слова содержатся в списке: "{}"' .format(result))
